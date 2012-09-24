ae1e558096ae9645c10256367eddb7821a35b9f5
Patrick <patrick@patrick-Aspire-8930.(none)>
2012-09-25 00:25:27 +0200

    Code opgeruimd met tabs en overbodige spaties verwijderd

--- Cserver/viaServer/main.c
+++ Cserver/viaServer/main.c
@@ -5,12 +5,12 @@
 
 
 
-typedef struct 
+typedef struct
 {
-       int id;                  
+       int id;
        char name[20];           //Sensor_X
        int value;               //0 or 1
-       char unit[20];             
+       char unit[20];
        char timestamp[22];        //2012-12-31_12:59:59
        int is_alarm;         //True:False
 }BinSen;
@@ -19,7 +19,7 @@ typedef struct
 BinSen test[12];
 
 int initializeDatabase()
-{   
+{
 
 
       int isAlarm = 0;
@@ -28,16 +28,16 @@ int initializeDatabase()
               test[i].id = i;
               test[i].is_alarm = isAlarm;
               if(isAlarm == 0){
-                         isAlarm =1;        
+                         isAlarm =1;
               }else{
-                         isAlarm =0;      
+                         isAlarm =0;
               }
-      }  
-      
+      }
+
       for(i = 0; i<12; i++){
-              printf("%d %d\n",test[i].id,test[i].is_alarm); 
-      }     
-    
+              printf("%d %d\n",test[i].id,test[i].is_alarm);
+      }
+
 }
 
 char* createValueString(){
@@ -45,169 +45,162 @@ char* createValueString(){
     char send[50];
     for(i = 0; i < 12; i++){
           send = test[i];
-          } 
+          }
     return send;
 }
 
 int main()
 {
-    initializeDatabase();
-    
-WORD wVersionRequested;
-WSADATA wsaData;
-int wsaerr;
- 
-// Using MAKEWORD macro, Winsock version request 2.2
-wVersionRequested = MAKEWORD(2, 2);
- 
-wsaerr = WSAStartup(wVersionRequested, &wsaData);
-if (wsaerr != 0)
-{
-    /* Tell the user that we could not find a usable */
-    /* WinSock DLL.*/
-    printf("The Winsock dll not found!\n");
-    return 0;
-}
-else
-{
-       printf("The Winsock dll found!\n");
-       printf("The status: %s.\n", wsaData.szSystemStatus);
-}
- 
-/* Confirm that the WinSock DLL supports 2.2.*/
-/* Note that if the DLL supports versions greater    */
-/* than 2.2 in addition to 2.2, it will still return */
-/* 2.2 in wVersion since that is the version we      */
-/* requested.                                        */
-if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
-{
-    /* Tell the user that we could not find a usable */
-    /* WinSock DLL.*/
-    printf("The dll do not support the Winsock version %u.%u!\n", LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
-    WSACleanup();
-    return 0;
-}
-else
-{
-    printf("The dll supports the Winsock version %u.%u!\n", LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
-    printf("The highest version this dll can support: %u.%u\n", LOBYTE(wsaData.wHighVersion), HIBYTE(wsaData.wHighVersion));
-}
- 
-//////////Create a socket////////////////////////
-//Create a SOCKET object called m_socket.
-SOCKET m_socket;
- 
-// Call the socket function and return its value to the m_socket variable.
-// For this application, use the Internet address family, streaming sockets, and
-// the TCP/IP protocol.
-// using AF_INET family, TCP socket type and protocol of the AF_INET - IPv4
-m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
- 
-// Check for errors to ensure that the socket is a valid socket.
-if (m_socket == INVALID_SOCKET)
-{
-    printf("Error at socket(): %ld.\n", WSAGetLastError());
-    WSACleanup();
-    return 0;
-}
-else
-{
-    printf("socket() is OK!\n");
-}
- 
-////////////////bind()//////////////////////////////
-// Create a sockaddr_in object and set its values.
-SOCKADDR_IN service;
- 
-// AF_INET is the Internet address family.
-service.sin_family = AF_INET;
-// "127.0.0.1" is the local IP address to which the socket will be bound.
-service.sin_addr.s_addr = inet_addr("127.0.0.1");
-// 55555 is the port number to which the socket will be bound.
-service.sin_port = htons(55555);
- 
-// Call the bind function, passing the created socket and the sockaddr_in structure as parameters.
-// Check for general errors.
- 
-if (bind(m_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
-{
-    printf("bind() failed: %ld.\n", WSAGetLastError());
-    closesocket(m_socket);
-    return 0;
-}
-else
-{
-   printf("bind() is OK!\n");
-}
+    	initializeDatabase();
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int wsaerr;
 
-if (listen( m_socket, 1) == SOCKET_ERROR)
-    printf("listen(): Error listening on socket %ld.\n", WSAGetLastError());
-else
-{
-    printf("listen() is OK, I'm waiting for connections...\n");
-}
-// Create a temporary SOCKET object called AcceptSocket for accepting connections.
-SOCKET AcceptSocket;
- 
-// Create a continuous loop that checks for connections requests. If a connection
-// request occurs, call the accept function to handle the request.
-printf("Server: Waiting for a client to connect...\n");
-printf("***Hint: Server is ready...run your client program...***\n");
-// Do some verification...
-while (1)
-{
-    AcceptSocket = SOCKET_ERROR;
- 
-      while (AcceptSocket == SOCKET_ERROR)
-       {
-        AcceptSocket = accept(m_socket, NULL, NULL);
-       }
- 
-   // else, accept the connection...
-   // When the client connection has been accepted, transfer control from the
-   // temporary socket to the original socket and stop checking for new connections.
-    printf("Server: Client Connected!\n");
-    m_socket = AcceptSocket;
-    break;
-}
+	// Using MAKEWORD macro, Winsock version request 2.2
+	wVersionRequested = MAKEWORD(2, 2);
 
+	wsaerr = WSAStartup(wVersionRequested, &wsaData);
+	if (wsaerr != 0)
+	{
+    		/* Tell the user that we could not find a usable */
+   		/* WinSock DLL.*/
+    		printf("The Winsock dll not found!\n");
+    		return 0;
+	}
+	else
+	{
+       		printf("The Winsock dll found!\n");
+       		printf("The status: %s.\n", wsaData.szSystemStatus);
+	}
+
+	/* Confirm that the WinSock DLL supports 2.2.*/
+	/* Note that if the DLL supports versions greater    */
+	/* than 2.2 in addition to 2.2, it will still return */
+	/* 2.2 in wVersion since that is the version we      */
+	/* requested.                                        */
+	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
+	{
+    		/* Tell the user that we could not find a usable */
+    		/* WinSock DLL.*/
+    		printf("The dll do not support the Winsock version %u.%u!\n", LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
+    		WSACleanup();
+    		return 0;
+	}
+	else
+	{
+    		printf("The dll supports the Winsock version %u.%u!\n", LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
+    		printf("The highest version this dll can support: %u.%u\n", LOBYTE(wsaData.wHighVersion), HIBYTE(wsaData.wHighVersion));
+	}
+
+	//////////Create a socket////////////////////////
+	//Create a SOCKET object called m_socket.
+	SOCKET m_socket;
+
+	// Call the socket function and return its value to the m_socket variable.
+	// For this application, use the Internet address family, streaming sockets, and
+	// the TCP/IP protocol.
+	// using AF_INET family, TCP socket type and protocol of the AF_INET - IPv4
+	m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// Check for errors to ensure that the socket is a valid socket.
+	if (m_socket == INVALID_SOCKET)
+	{
+    		printf("Error at socket(): %ld.\n", WSAGetLastError());
+    		WSACleanup();
+    		return 0;
+	}
+	else
+	{
+    		printf("socket() is OK!\n");
+	}
+
+	////////////////bind()//////////////////////////////
+	// Create a sockaddr_in object and set its values.
+	SOCKADDR_IN service;
+
+	// AF_INET is the Internet address family.
+	service.sin_family = AF_INET;
+	// "127.0.0.1" is the local IP address to which the socket will be bound.
+	service.sin_addr.s_addr = inet_addr("127.0.0.1");
+	// 55555 is the port number to which the socket will be bound.
+	service.sin_port = htons(55555);
+
+	// Call the bind function, passing the created socket and the sockaddr_in structure as parameters.
+	// Check for general errors.
+
+	if (bind(m_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
+	{
+    		printf("bind() failed: %ld.\n", WSAGetLastError());
+    		closesocket(m_socket);
+   	 	return 0;
+	}
+	else
+	{
+   		printf("bind() is OK!\n");
+	}
+
+	if (listen( m_socket, 1) == SOCKET_ERROR)
+    		printf("listen(): Error listening on socket %ld.\n", WSAGetLastError());
+	else
+	{
+    		printf("listen() is OK, I'm waiting for connections...\n");
+	}
+	// Create a temporary SOCKET object called AcceptSocket for accepting connections.
+	SOCKET AcceptSocket;
+
+	// Create a continuous loop that checks for connections requests. If a connection
+	// request occurs, call the accept function to handle the request.
+	printf("Server: Waiting for a client to connect...\n");
+	printf("***Hint: Server is ready...run your client program...***\n");
+	// Do some verification...
+	while (1)
+	{
+    		AcceptSocket = SOCKET_ERROR;
+
+      		while (AcceptSocket == SOCKET_ERROR)
+       		{
+        		AcceptSocket = accept(m_socket, NULL, NULL);
+       		}
+
+   		// else, accept the connection...
+   		// When the client connection has been accepted, transfer control from the
+   		// temporary socket to the original socket and stop checking for new connections.
+    		printf("Server: Client Connected!\n");
+    		m_socket = AcceptSocket;
+    		break;
+	}
 
 
-while(1){
-         int bytesSent = SOCKET_ERROR;
-         int bytesRecv = SOCKET_ERROR;
-         char sendbuf[100] = "test\n";
-         char recvbuf[100] = "";
-         char boem[] = "quit";
-         
+
+	while(1){
+         	int bytesSent = SOCKET_ERROR;
+         	int bytesRecv = SOCKET_ERROR;
+         	char sendbuf[100] = "test\n";
+         	char recvbuf[100] = "";
+         	char boem[] = "quit";
+
          while(1){
                   printf("Server: Ready to receive! \n");
                   bytesRecv = recv(m_socket, recvbuf, 100, 0);
                   if(bytesRecv != SOCKET_ERROR){
-                               
                                if(!strncmp(recvbuf,boem, 4)){
-                                       exit(EXIT_SUCCESS);                                                                       
+                                       exit(EXIT_SUCCESS);
                                }
-                               
-                            
-                               
                                break;
                   }
          }
          while(1){
-                  bytesSent = send(m_socket, sendbuf, 100, 0);         
-                  if (bytesSent != SOCKET_ERROR){                                  
-                                break;      
-                  } 
-            
-         }         
-         
+                  bytesSent = send(m_socket, sendbuf, 100, 0);
+                  if (bytesSent != SOCKET_ERROR){
+                                break;
+                  }
+         }
 }
 
- 
+
 WSACleanup();
-  
-  system("PAUSE");	
-  return 0;
+
+ 	system("PAUSE");
+  	return 0;
 }
 
